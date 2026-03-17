c96b6e77c236f2963f0eea99425381be38233e1c
nestor-puerta <nestorpuerta2006@gmail.com>
2026-03-17 16:12:59 -0500

    Primer commit: Estructura Proyecto

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import Layout from './components/layout';
+import Footer from './components/footer';
+import Rutas from './components/rutas';
+
+import './App.css';
+
+function App() {
+    return (
+        <>
+            <Layout />
+            <Rutas />
+            <Footer />
+        </>
+    );
+}
+
+export default App;
---
+++ src/components/rutas.jsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import { Routes, Route } from 'react-router-dom';
+import Home from '../pages/Home';
+import About from '../pages/About';
+import PerfumesHombre from '../pages/PerfumesHombre';
+import PerfumesMujer from '../pages/PerfumesMujer';
+import PerfumesNinos from '../pages/PerfumesNinos';
+import PerfumesMascotas from '../pages/PerfumesMascotas';
+
+function Rutas() {
+  return (
+    <div className="App">
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/perfumes-hombre" element={<PerfumesHombre />} />
+        <Route path="/perfumes-mujer" element={<PerfumesMujer />} />
+        <Route path="/perfumes-ninos" element={<PerfumesNinos />} />
+        <Route path="/perfumes-mascotas" element={<PerfumesMascotas />} />
+      </Routes>
+    </div>
+  );
+}
+
+export default Rutas;
